2b73e784b830e7f18acc16af0307f16ee46b4815
simön <simon.ledezma2005@gmail.com>
2025-04-04 21:58:12 -0300

    Codigo Final

--- Problemas Resueltos/Problema 2/Codigo.py
+++ Problemas Resueltos/Problema 2/Codigo.py
@@ -1,1 +1,8 @@
+# Solicitud de valor en centimetros
+valor = float(input("Ingrese el valor en centimetros: "))
 
+# Conversion a metros  
+valor_metros = valor / 100
+
+# Impresion del resultado
+print(f"El valor en metros es: {valor_metros} m")
