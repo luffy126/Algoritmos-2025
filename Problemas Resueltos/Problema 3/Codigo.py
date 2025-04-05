65d6918911a2bd85fcb15b8f5675be1e59ea572e
simön <simon.ledezma2005@gmail.com>
2025-04-04 23:42:54 -0300

    Codigo Final

--- Problemas Resueltos/Problema 3/Codigo.py
+++ Problemas Resueltos/Problema 3/Codigo.py
@@ -1,1 +1,12 @@
+# Area de un triangulo
+# Solicitud de base y altura
+
+base = float(input("Ingrese la base del triangulo: "))
+altura = float(input("Ingrese la altura del triangulo: "))
+
+# Calculo del area
+area = (base * altura) / 2
+
+# Impresion del resultado
+print(f"El area del triangulo es: {area} m2")
 
--- Problemas Resueltos/Problema 4/Codigo.py
+++ Problemas Resueltos/Problema 4/Codigo.py
@@ -1,1 +1,11 @@
+# Solicitud de tres notas
+nota1 = float(input("Ingrese la primera nota: "))
+nota2 = float(input("Ingrese la segunda nota: "))
+nota3 = float(input("Ingrese la tercera nota: "))
+
+# Calculo del promedio
+promedio = (nota1 * 0.3 + nota2 * 0.35 + nota3 * 0.35) 
+
+# Impresion del resultado   
+print(f"El promedio de las notas es: {promedio}")
 
