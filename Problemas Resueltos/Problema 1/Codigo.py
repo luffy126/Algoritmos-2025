b3a6a4005aa684833ffac0459669f1ad233b81e6
Tomás Monge <125072737+TenerexBlatta@users.noreply.github.com>
2025-04-04 21:39:40 -0300

    Update

--- Problemas Resueltos/Problema 1/Codigo.py
+++ Problemas Resueltos/Problema 1/Codigo.py
@@ -1,1 +1,11 @@
-#WIP
+# Solicitar el primer número
+numero1 = float(input("Ingresa el primer número: "))
+
+# Solicitar el segundo número
+numero2 = float(input("Ingresa el segundo número: "))
+
+# Calcular la suma
+resultado = numero1 + numero2
+
+# Mostrar el resultado
+print(f"La suma de {numero1} y {numero2} es: {resultado}")
